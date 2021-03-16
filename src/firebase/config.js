761f0e8c6ccb2a31060e2dbd4d3245b71d530a50
rjmcbain <rmcbain01@gmail.com>
2021-03-16 12:55:04 -0700

    adding firebase/firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDV9f9-WupJaUnuDFWlk66c06VtCGUDJXo",
+  authDomain: "music-playlist-fa074.firebaseapp.com",
+  projectId: "music-playlist-fa074",
+  storageBucket: "music-playlist-fa074.appspot.com",
+  messagingSenderId: "1080351879002",
+  appId: "1:1080351879002:web:0c9f0889208a5ea6d9cd63",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+// Global Styles
+import "./assets/main.css";
+
+createApp(App)
+  .use(router)
+  .mount("#app");
